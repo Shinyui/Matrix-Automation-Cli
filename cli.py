e700ae3663151a8d4ef3ba7d393ee71c68bbe7c2
Shinyui Chu <shinyui@Shinyuis-MacBook-Pro.local>
2025-07-22 03:22:12 +0800

    update cli command

--- cli.py
+++ cli.py
@@ -1,38 +1,50 @@
-import sys
 import argparse
-from simulate_home.devices import get_device_info_list, export_to_csv
-from simulate_home.runner import run_all_devices
+from utils.device import ADBDeviceManager
+from instagram.simulate_home_activity.concurrent_runner import run_all_devices as instagram_simulate_home
+from threads.simulate_home_activity.concurrent_runner import run_all_devices as threads_simulate_home
 
 def main():
     parser = argparse.ArgumentParser(
-        description="📱 Instagram 自動化控制工具",
+        description="Matrix Automation Cli",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
-    subparsers = parser.add_subparsers(dest="command", help="可用指令")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
-    scan_parser = subparsers.add_parser("scan", help="🔍 掃描目前已連接的 ADB 裝置並匯出 CSV")
+    scan_parser = subparsers.add_parser("device", help="Scan devices")
+    scan_parser.add_argument("--mode", type=str, help="Scan mode")
     scan_parser.add_argument("--path", type=str, help="ADB path")
-    scan_parser.add_argument("--output", type=str, default="scanned_devices.csv", help="輸出檔案名稱")
+    scan_parser.add_argument("--file", type=str, help="Output file name")
 
-    simulate_parser = subparsers.add_parser("simhome", help="🤖 開始模擬所有 devices.csv 裡的裝置操作")
-    simulate_parser.add_argument("--device", type=str, default="devices.csv", help="欲載入的裝置清單 CSV")
-    scan_parser.add_argument("--step", type=int, default=100, help="執行步驟")
+    insta_parser = subparsers.add_parser("instagram", help="Run Instagram simulation")
+    insta_parser.add_argument("--mode", type=str, help="Automation mode")
+    insta_parser.add_argument("--device", type=str, help="Device list")
+    insta_parser.add_argument("--step", type=int, default=100, help="Simulation steps")
 
+    threads = subparsers.add_parser("threads", help="Run Threads simulation")
+    threads.add_argument("--mode", type=str, help="Automation mode")
+    threads.add_argument("--device", type=str, help="Device list")    
+    threads.add_argument("--step", type=int, default=100, help="Simulation steps")
 
     args = parser.parse_args()
 
-    if args.command == "scan":
-        print("🔍 正在掃描裝置...")
-        sys.stdout.flush()
-        device_info = get_device_info_list(adb_path=args.path)
-        export_to_csv(device_info, filename=args.output)
-    elif args.command == "simulate":
-        print("🤖 開始模擬操作...")
-        sys.stdout.flush()
+    if args.command == "device":
+        if args.mode == "scan":
+            print("scanning devices", flush=True)
+            device = ADBDeviceManager(args.path)
+            device_info = device.get_device_info_list()
+            device.export_to_csv(device_info_list=device_info,filename=args.file)
+    
+    elif args.command == "instagram":
+        if args.mode == "home":
+            instagram_simulate_home(device_csv_path=args.device, step=args.step)
+        elif args.mode == "reels":
+            print("simulate instagram reels")
 
-        run_all_devices(device_csv_path=args.csv)
-    else:
-        parser.print_help()
+    elif args.command == "threads":
+        if args.mode == "home":
+            threads_simulate_home(device_csv_path=args.device, step=args.step)
+        elif args.mode == "reels":
+            print("simulate threads reels")
 
 if __name__ == "__main__":
     main()
